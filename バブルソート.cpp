e6d72cd123481277c8219c864b4cc9c1404cc17b
sk2k0 <sk2k0@MSI>
2023-10-27 14:04:07 +0900

    プロジェクト ファイルを追加します。

---
+++ バブルソート.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void InitRand()
+{
+	srand((unsigned int)time(NULL));
+}
+
+void InitRand(int* array, int arraysize)
+{
+	for (int i = 0; i < arraysize; ++i)
+	{
+		*(array + i) = rand() % 99 + 1;
+	}
+}
+
+void Show(const int* const array, const int const arraysize)
+{
+	for (int i = 0; i < arraysize; ++i)
+	{
+		cout << *(array + i) << " " << flush;
+	}
+}
+
+void BubbleSort(int* array, int arraysize)
+{
+	for (int i = 1; i <= arraysize; ++i)
+	{
+		int keep = 0;
+		for (int j = arraysize - 1; 0 < j; --j)
+		{
+			if (*(array + j) < *(array + j - 1))
+			{
+				keep = *(array + j);
+				*(array + j) = *(array + j - 1);
+				*(array + j - 1) = keep;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int arraysize;
+	cout << "配列の大きさを決めてください > " << flush;
+	cin >> arraysize;
+
+	int* array = new int[arraysize];
+
+	if (array == NULL)
+	{
+		*array = 0;
+
+		cout << *array << endl;
+		delete[] array;
+	}
+
+	InitRand();
+	InitRand(array, arraysize);
+
+	cout << "バブルソートをする前 " << flush;
+	Show(array, arraysize);
+	cout << endl;
+
+	BubbleSort(array, arraysize);
+
+	cout << "バブルソートをした後 " << flush;
+	Show(array, arraysize);	
+	cout << endl;
+}
